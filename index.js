4c311144333a898ce598c6b9f3c5e708483b9c7b
AAnn1912 <phamthienan862@gmail.com>
2023-01-26 19:35:18 +0700

    finnish-window

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ function swItchLight() {
   moonHide.className = "moon";
   sunHide.className = "sun-hide";
 }
+
+window.onscroll = function () {
+  var scroll = window.scrollY;
+
+  if (scroll >= 80) {
+    document.querySelector("#menu").classList.add("nav-fixed");
+  } else {
+    document.querySelector("#menu").classList.remove("nav-fixed");
+  }
+};
